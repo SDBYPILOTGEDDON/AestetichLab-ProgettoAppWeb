e886b2a7c899ed01953806a2e373d558930f306a
SDBYPLTGDDN <Mattia.leta01@icatt.it>
2022-03-23 21:49:58 +0100

    Aggiungo Carousel e Colori nuovi

--- js/main.js
+++ js/main.js
@@ -7,5 +7,16 @@ $(document).ready(function(){
 
     $toggleCollaspe.click(function(){
         $nav.toggleClass('collapse');
+        
     })
+
+    // owl-carousel 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplauTimeout: 3000,
+        dots:false,
+        nav:true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
 });
